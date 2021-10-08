7ba79e31a4b47bb24fddb9763d66999d28d7b04f
JCompSci <91136966+JCompSci@users.noreply.github.com>
2021-10-08 08:56:31 -0500

    Update simonSoundPanel.py

--- Sounds/simonSoundPanel.py
+++ Sounds/simonSoundPanel.py
@@ -6,34 +6,35 @@ from pygame import *
 #Inits the Mixer functions
 pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512, devicename= "Built-in Output", allowedchanges=AUDIO_ALLOW_FREQUENCY_CHANGE | AUDIO_ALLOW_CHANNELS_CHANGE)
 
-def soundGreen():
+def soundColor(color):
 
-    #creates the sound object
-    greenSound = pygame.mixer.Sound("D_01.ogg")
+    if(color == 0):
+        #creates the sound object
+        greenSound = pygame.mixer.Sound("D_01.ogg")
 
-    #plays the sound object
-    pygame.mixer.Sound.play(greenSound, maxtime = 400)
+        #plays the sound object
+        pygame.mixer.Sound.play(greenSound, maxtime = 400)
 
-def soundRed():
 
-    #creates the sound object
-    redSound = pygame.mixer.Sound("F_01.ogg")
+    if(color == 1):
+        #creates the sound object
+        redSound = pygame.mixer.Sound("F_01.ogg")
 
-    #plays the sound object
-    pygame.mixer.Sound.play(redSound, maxtime = 400)
+        #plays the sound object
+        pygame.mixer.Sound.play(redSound, maxtime = 400)
 
-def soundYellow():
 
-    #creates the sound object
-    yellowSound = pygame.mixer.Sound("A_01.ogg")
+    if(color == 2):
+        #creates the sound object
+        yellowSound = pygame.mixer.Sound("A_01.ogg")
 
-    #plays the sound object
-    pygame.mixer.Sound.play(yellowSound, maxtime = 400)
+        #plays the sound object
+        pygame.mixer.Sound.play(yellowSound, maxtime = 400)
 
-def soundBlue():
 
-    #creates the sound object
-    blueSound = pygame.mixer.Sound("C_01.ogg")
+    if(color == 3):
+        #creates the sound object
+        blueSound = pygame.mixer.Sound("C_01.ogg")
 
-    #plays the sound object
-    pygame.mixer.Sound.play(blueSound, maxtime = 400)
+        #plays the sound object
+        pygame.mixer.Sound.play(blueSound, maxtime = 400)
