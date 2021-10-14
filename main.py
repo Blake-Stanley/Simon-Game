cb014abf9d57aebd1f773f29757b2aec3d466354
Blake Stanley <91094385+Blake-Stanley@users.noreply.github.com>
2021-10-14 09:24:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,7 @@ buttonR = button.Button(335, 0, 672, 335, "Red")
 buttonY = button.Button(0, 335, 335, 672, "Yellow")
 buttonB = button.Button(335, 335, 672, 672, "Blue")
 
-buttonG.draw(screen, 0, 200, 0)
-buttonR.draw(screen, 200, 0, 0)
-buttonY.draw(screen, 200, 70, 50)
-buttonB.draw(screen, 0, 0, 200)
-screen.blit(mainPNG, (0, 0))
+
 
 pg.display.flip()
 
@@ -57,48 +53,86 @@ def runSequence():
         simonSoundPanel.soundColor(logic.getSequence()[i])
         pg.time.delay(300)
 
+def mainGame():
+    buttonG.draw(screen, 0, 200, 0)
+    buttonR.draw(screen, 200, 0, 0)
+    buttonY.draw(screen, 200, 70, 50)
+    buttonB.draw(screen, 0, 0, 200)
+    screen.blit(mainPNG, (0, 0))
+    # add value to sequence to create first sequence
+    logic.progress()
+    runSequence()
 
-# add value to sequence to create first sequence
-logic.progress()
-runSequence()
+    # variable tracks whether the button has been clicked so that if you hold down mouseclick it only clicks once
+    allowClick = True
+    loop = True
+    currentColor = ""
+    while loop:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                loop = False
 
-# variable tracks whether the button has been clicked so that if you hold down mouseclick it only clicks once
-allowClick = True
-loop = True
-currentColor = ""
-while loop:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            loop = False
+        if event.type == pg.MOUSEBUTTONDOWN and allowClick is True:
+            allowClick = False
+            pos = pg.mouse.get_pos()
+            if buttonG.isClicked(pos):
+                currentColor = "g"
+            elif buttonR.isClicked(pos):
+                currentColor = "r"
+            elif buttonY.isClicked(pos):
+                currentColor = "y"
+            elif buttonB.isClicked(pos):
+                currentColor = "b"
 
-    if event.type == pg.MOUSEBUTTONDOWN and allowClick is True:
-        allowClick = False
-        pos = pg.mouse.get_pos()
-        if buttonG.isClicked(pos):
-            currentColor = "g"
-        elif buttonR.isClicked(pos):
-            currentColor = "r"
-        elif buttonY.isClicked(pos):
-            currentColor = "y"
-        elif buttonB.isClicked(pos):
-            currentColor = "b"
+        if event.type == pg.MOUSEBUTTONUP and not allowClick:
+            flash(currentColor)
+            simonSoundPanel.soundColor(currentColor)
+            logic.appendPlayerSequence(currentColor)
+            if logic.check() is False:
+                print("you failed")  # bring to loss screen instead | lossPage()
+                loop = False
+            print("iterated")
 
-    if event.type == pg.MOUSEBUTTONUP and not allowClick:
-        flash(currentColor)
-        simonSoundPanel.soundColor(currentColor)
-        logic.appendPlayerSequence(currentColor)
-        if logic.check() is False:
-            print("you failed")  # bring to lose screen instead
-            loop = False
-        print("iterated")
+            if len(logic.getPlayerSequence()) == len(logic.getSequence()):
+                logic.clearPlayerSequence()
+                logic.progress()
+                runSequence()
+                print("running sequence")
+            allowClick = True
 
-        if len(logic.getPlayerSequence()) == len(logic.getSequence()):
-            logic.clearPlayerSequence()
-            logic.progress()
-            runSequence()
-            print("running sequence")
-        allowClick = True
+title1 = pg.image.load("KGD Logo frame 1.png")
+title2 = pg.image.load("kgd logo frame 2.png")
+title3 = pg.image.load("kgd frame 3.png")
+title4 = pg.image.load("kgd frame 4.png")
+title5 = pg.image.load("kgd frame 5.png")
 
+titles = [title1, title2, title3, title4, title5]
+
+def titleScreen():
+    for title in titles:
+        screen.blit(title, (0, 0))
+        pg.display.update()
+        pg.time.delay(200)
+    titles.reverse()
+    for title in titles:
+        screen.blit(title, (0, 0))
+        pg.display.update()
+        pg.time.delay(200)
+
+def menu():
+    pass
+
+def lossPage():
+    pass # don't forget to add high score logic in here 
+
+def main():
+    titleScreen()
+    titleScreen()
+    titleScreen()
+    mainGame()
+
+if __name__ == "__main__":
+    main()
 
 pg.quit()
 quit()
